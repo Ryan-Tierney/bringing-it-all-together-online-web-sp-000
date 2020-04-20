8e1c9c0320e5e2ed159b7d147c2ae8edf60f1a27
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-20 19:50:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,7 @@ class Dog
    dog 
   end 
   
-  def self.new_from_db
+  def self.find_by_id
     sql = <<-SQL
     SELECT * FROM dogs WHERE id = ?
     SQL
@@ -55,6 +55,13 @@ class Dog
   end 
   
   def self.find_by_name 
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE name = ? 
+    SQL
+    
+  end 
+  
+  def self.new_from_db
   end 
   
   def update 
