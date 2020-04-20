8ef9ffb90d65ceceb0f1b7ce1c89d094bfa9220b
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-20 19:37:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,7 +30,7 @@ class Dog
   
    def save 
     sql = <<-SQL 
-     INSERT INTO students (name, breed) 
+     INSERT INTO dogs (name, breed) 
      VALUES (?, ?)
      SQL
      DB[:conn].execute(sql, self.name, self.breed)
