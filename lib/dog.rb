e862286501efd55eee30c563a54cf820526fed8b
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-20 19:25:06 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,44 @@
+class Dog 
+  
+  attr_accessor :name, :breed, :id
+  
+  def initialize(id: id=nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed 
+  end 
+  
+  def self.create_table 
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS dogs ( 
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      breed TEXT 
+      );
+      SQL
+      
+      DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table 
+    sql = <<-SQL 
+    DROP TABLE IF EXISTS dogs 
+    SQL
+    
+    DB[:conn].execute(sql)
+  end 
+  
+  def save 
+    
+  end 
+  
+  def self.new_from_db
+  end 
+  
+  def self.find_by_name 
+  end 
+  
+  def update 
+  end 
+    
+end 
