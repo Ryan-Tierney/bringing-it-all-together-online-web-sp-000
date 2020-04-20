7a35fc9441db762983ea5c7d661e9b619561e3cb
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-20 19:52:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,6 +52,9 @@ class Dog
     SELECT * FROM dogs WHERE id = ?
     SQL
     
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end 
   
   def self.find_by_name(name)
