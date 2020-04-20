b8e68ba4e690cb79dc5e1a0a255f687fcca191d7
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-20 19:37:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,8 +28,14 @@ class Dog
     DB[:conn].execute(sql)
   end 
   
-  def save
-    
+   def save 
+    sql = <<-SQL 
+     INSERT INTO students (name, breed) 
+     VALUES (?, ?)
+     SQL
+     DB[:conn].execute(sql, self.name, self.breed)
+     
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end 
   
   def self.new_from_db
