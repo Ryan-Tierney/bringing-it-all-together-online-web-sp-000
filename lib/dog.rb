96d4b7861ecf7c64b471e4e543c9611c3daf01d5
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-20 19:56:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,11 +66,12 @@ class Dog
   
   def self.new_from_db(row)
     attributes_hash = {
-      :id = row[0],
-      :name = roww[1],
-      :breed = row[2]
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
     }
-  end 
+    self.new(attributes_hash)
+  end
   
   def update 
   end 
